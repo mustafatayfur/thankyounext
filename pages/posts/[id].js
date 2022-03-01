b54588c5ff3798aacb76a322f66635ce22e5e41f
mustafa <mltm0204@gmail.com>
2022-03-01 22:00:27 +0300

    getStaticProps

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -9,15 +9,47 @@ const PostDetails = ({post}) => {
   )
 }
 
-export const getServerSideProps = async (context)=> {
+// STATIC GENERATION
+
+export const getStaticPaths = async () => {
+    const res = await fetch (`https://jsonplaceholder.typicode.com/posts/`)
+    const posts = await res.json()
+
+    const paths = posts.map(post => {
+        return {
+            params: { id: post.id.toString()}
+        }
+    })
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export const getStaticProps = async (context) => {
     const res = await fetch (`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
 
     const post = await res.json()
 
     return {
-        props: {
-            post,
-        }
+       props: {
+           post,
+       }
     }
 }
+
+// SERVER-SIDE RENDERING
+
+// export const getServerSideProps = async (context)=> {
+//     const res = await fetch (`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+
+//     const post = await res.json()
+
+//     return {
+//         props: {
+//             post,
+//         }
+//     }
+// }
 export default PostDetails
