4cdcd073cf20d90e1a2ce3515029abf13608677d
mustafa <mltm0204@gmail.com>
2022-03-01 16:09:54 +0300

    ...

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import styles from "../styles/Layout.module.css"
+import Nav from './Nav'
 
 
 const Layout = ({children}) => {
   return (
+    <>
+    <Nav/>
     <div className={styles.container}>
         <main className={styles.main}>
-            <h1>Layout bütün sayfalarda görünür</h1>
-            {children}
+        <h1>Layout bütün sayfalarda görünür</h1>
+        {children}
         </main>
     </div>
+    </>
+    
   )
 }
 
