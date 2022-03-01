dfc2a6741633e8c9e1528c51e7cddbebe08509f6
mustafa <mltm0204@gmail.com>
2022-03-01 14:35:17 +0300

    ...

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,13 @@
+import Head from "next/head"
+
 export default function About() {
     return (
         <div>
-                <h1>About</h1>
+            <Head>
+                <title>About Page</title>
+                <link rel="icon" href="/vercel.svg" />    
+            </Head>
+            <h1>About</h1>
         </div>
     )
 }
