9fa3da93e890fcefbccd3a1127a2cad9c9605684
mustafa <mltm0204@gmail.com>
2022-03-02 21:08:28 +0300

    ...

--- pages/api/videos/index.js
+++ pages/api/videos/index.js
@@ -1,15 +1,6 @@
 
 import { videos } from "../../../videos"
+
 export default (req, res) => {
-  const {id} = req.query
-
-  const video = videos.find((video) => video.id.toString() === id)
-
-  if (video){
-    res.status(200).json(video)
-  }else {
-    res.status(404).json({
-      message: "Error. No such video!"
-    })
-  }
+  res.status(200).json(videos)
 }
